2bf5ae2eeb6e393d7c53effef528685529d5e4c8
Jesse Vaerendonck <jesse.vaerendonck@student.hogent.be>
2025-04-11 15:18:47 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ Reagent Game Jam 2025/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;  
+using UnityEngine.UI;  
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject settingsScreen;
+    public GameObject menuScreen;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main");  
+    }
+    
+    public void ShowSettings()
+    {
+        menuScreen.SetActive(false);
+        settingsScreen.SetActive(true);  
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("Game wordt gesloten!");
+        Application.Quit();
+    }
+
+    public void CloseSettings()
+    {
+        settingsScreen.SetActive(false);
+        menuScreen.SetActive(true);
+    }
+}
