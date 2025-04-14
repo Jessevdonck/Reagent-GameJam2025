991924f2105a2d362a9fd4e2ef5fb0e385cc2ef3
Jordan Bentkowski <jordan.bentkowski@student.hogent.be>
2025-04-14 21:32:36 +0200

    Counting spel klaar

---
+++ Reagent Game Jam 2025/Assets/Scripts/MiniGame/Counting/CounterGame.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace MiniGame
+{
+
+
+    public class CounterGame : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> prefabs;
+        private Vector2 startPosition = new Vector2(-25f, 5f);
+        public float xSpacing = 10f;
+        public float ySpacing = 10f;
+        
+        private static CounterGame instance;
+        private int count;
+
+        public int getCount()
+        {
+            return count;
+        }
+        
+        private bool canClick;
+
+        public bool getCanClick()
+        {
+            return canClick;
+        }
+
+        private void Update()
+        {
+            if (count == 12)
+            {
+                Debug.Log("finish");
+            }
+        }
+
+        void Start()
+        {
+            count = 0;
+            spawnCounterComponents();
+            canClick = true;
+        }
+
+        void spawnCounterComponents()
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 12; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Shuffle(numbers);
+            
+           
+            
+
+            
+            for (int i = 0; i < 12; i++)
+            {
+                int row = i / 6; // 0 or 1
+                int col = i % 6;
+
+                Vector2 spawnPos = new Vector2(
+                    startPosition.x + col * xSpacing,
+                    startPosition.y - row * ySpacing
+                );
+                
+                GameObject instance = Instantiate(prefabs[numbers[i]], spawnPos, Quaternion.identity);
+                CounterComponent counter = instance.GetComponent<CounterComponent>();
+                
+            }
+        }
+        
+        void Shuffle(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                int randomIndex = Random.Range(i, list.Count);
+                (list[i], list[randomIndex]) = (list[randomIndex], list[i]);
+            }
+        }
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        public static CounterGame getInstance()
+        {
+            return instance;
+        }
+
+        public void nextCount()
+        {
+            count += 1;
+        }
+
+        public void wrongCount()
+        {
+            StartCoroutine(DisableClickTemporarily());
+        }
+        
+        public IEnumerator DisableClickTemporarily()
+        {
+            Debug.Log("wrong");
+            canClick = false;
+            yield return new WaitForSeconds(3f);
+            canClick = true;
+        }
+
+        
+    }
+}
